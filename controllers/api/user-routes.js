001dd6de679ec09436fe9d3dae6d7b9e79b0cd69
zanderson1998 <zanderson1998@yahoo.com>
2021-02-25 19:28:22 -0700

    add login and logout route

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -72,6 +72,48 @@ router.post('/', (req, res) => {
         });
 });
 
+//log in route (.../api/users/login)
+router.post('/login', (req, res) => {
+    User.findOne({
+        where: {
+            email: req.body.email
+        }
+    }).then(dbUserData => {
+        if (!dbUserData) {
+            res.status(400).json({ message: 'No user with that email address' });
+            return;
+        }
+        //verify user
+        const validPassword = dbUserData.checkPassword(req.body.password);
+
+        if (!validPassword) {
+            res.status(400).json({ message: 'Incorrect password!' });
+            return;
+        }
+        req.session.save(() => {
+            //declare session variables
+            req.session.user_id = dbUserData.id;
+            req.session.username = dbUserData.username;
+            req.session.loggedIn = true;
+
+            res.json({ user: dbUserData, message: 'You are now logged in!' });
+        });
+        
+    });
+});
+
+//logout route (.../api/users/logout)
+router.post('/logout', (req, res) => {
+    if (req.session.loggedIn) {
+        req.session.destroy(() => {
+            res.status(204).end();
+        });
+    }
+    else {
+        res.status(404).end();
+    }
+});
+
 //update a user(.../api/users/:id)
 router.put('/:id', auth,(req, res) => {
     User.update(req.body, {
