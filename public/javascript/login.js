353e401c2696eeca1dffffaf0d24580ce033b404
mheyn95 <mheyn95@gmail.com>
2021-02-25 19:59:46 -0700

    refactored login js to be in jquery

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -1,31 +1,9 @@
-async function signupFormHandler(event) {
+//function that will run on submit of the login form, this will send the username and password and check if they exist, if they do they are signed in
+$(".login-form").on("submit", async function (event) {
   event.preventDefault();
 
-  const username = document.querySelector("#username-signup").value.trim();
-  const password = document.querySelector("#password-signup").value.trim();
-
-  if (username && password) {
-    await fetch("/api/users", {
-      method: "post",
-      body: JSON.stringify({
-        username,
-        password,
-      }),
-      headers: { "Content-Type": "application/json" },
-    });
-    if (response.ok) {
-      console.log("success");
-    } else {
-      alert(response.statusText);
-    }
-  }
-}
-
-async function loginFormHandler(event) {
-  event.preventDefault();
-
-  const username = document.querySelector("#username-login").value.trim();
-  const password = document.querySelector("#password-login").value.trim();
+  const username = $("#username-login").value.trim();
+  const password = $("#password-login").value.trim();
 
   if (username && password) {
     const response = await fetch("/api/users/login", {
@@ -43,12 +21,28 @@ async function loginFormHandler(event) {
       alert(response.statusText);
     }
   }
-}
+});
 
-document
-  .querySelector(".login-form")
-  .addEventListener("submit", loginFormHandler);
+//function that will run on submit of the signup form, this will send the username and password and create them in the database
+$(".signup-form").on("submit", async function (event) {
+  event.preventDefault();
 
-document
-  .querySelector(".signup-form")
-  .addEventListener("submit", signupFormHandler);
+  const username = $("#username-signup").value.trim();
+  const password = $("#password-signup").value.trim();
+
+  if (username && password) {
+    await fetch("/api/users", {
+      method: "post",
+      body: JSON.stringify({
+        username,
+        password,
+      }),
+      headers: { "Content-Type": "application/json" },
+    });
+    if (response.ok) {
+      console.log("success");
+    } else {
+      alert(response.statusText);
+    }
+  }
+});
