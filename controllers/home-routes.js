118dd8d6f64d3a607aa9c5a05ad51328d596b17b
zanderson1998 <zanderson1998@yahoo.com>
2021-03-02 11:27:57 -0700

    adjust styling

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -28,6 +28,7 @@ router.get('/', (req,res) => {
     })
         .then(dbPostData => {
             const posts = dbPostData.map(post => post.get({ plain: true }));
+            // const loc = req.session;
             res.render('homepage', { posts, loggedIn: req.session.loggedIn });
         })
         .catch(err => {
---
+++ public/javascript/script.js
@@ -0,0 +1,19 @@
+const activeTab = () => {
+    let loc = window.location.pathname;
+    switch(loc) {
+        case '/':
+            document.querySelector('.explore-tab').classList.add("active");
+            break;
+        case '/codeHelp':
+            document.querySelector('.code-help-tab').classList.add("active");
+            break;
+        case '/following':
+            document.querySelector('.following-tab').classList.add("active");
+            break;
+        case '/userpage':
+            document.querySelector('.home-tab').classList.add("active");
+            break;
+    }
+}
+
+activeTab();
