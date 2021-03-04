8a9a7c6d8514a958c0b20a216b200ba4c142b697
mheyn95 <mheyn95@gmail.com>
2021-03-04 12:37:00 -0700

    added like functionality

--- models/Post.js
+++ models/Post.js
@@ -1,48 +1,70 @@
-const { Model, DataTypes } = require('sequelize');
-const sequelize = require('../config/connection');
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
 
 class Post extends Model {
-    //insert custom functions here
+  static upvote(body, models) {
+    return models.Vote.create({
+      user_id: body.user_id,
+      post_id: body.post_id,
+    }).then(() => {
+      return Post.findOne({
+        where: {
+          id: body.post_id,
+        },
+        attributes: [
+          "id",
+          "body",
+          "title",
+          "created_at",
+          [
+            sequelize.literal(
+              "(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)"
+            ),
+            "vote_count",
+          ],
+        ],
+      });
+    });
+  }
 }
 
 //define table columns and how they are configured
 Post.init(
-    {
-        //unique id for each
-        id: {
-            type: DataTypes.INTEGER,
-            allowNull: false,
-            primaryKey: true,
-            autoIncrement: true
-        },
-        user_id: {
-            type: DataTypes.INTEGER,
-            allowNull: false,
-            references: {
-                model: 'user',
-                key: 'id'
-            }
-        },
-        title: {
-            type: DataTypes.STRING,
-            allowNull: false,
-        },
-        body: {
-            type: DataTypes.STRING(500),
-            allowNull: false
-        },
-        code_help: {
-            type: DataTypes.BOOLEAN,
-            allowNull: false
-        }
+  {
+    //unique id for each
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
     },
-    {
-        sequelize,
-        underscored: true,
-        freezeTableName: true,
-        modelName: 'post'
-    }
+    user_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: "user",
+        key: "id",
+      },
+    },
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    body: {
+      type: DataTypes.STRING(500),
+      allowNull: false,
+    },
+    code_help: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+    },
+  },
+  {
+    sequelize,
+    underscored: true,
+    freezeTableName: true,
+    modelName: "post",
+  }
 );
 
 module.exports = Post;
-
--- public/javascript/upvote.js
+++ public/javascript/upvote.js
@@ -1,17 +1,18 @@
 async function upvoteClickHandler(event) {
+  console.log("test");
   event.preventDefault();
 
-  const id = window.location.toString().split('/')[
-    window.location.toString().split('/').length - 1
+  const id = window.location.toString().split("/")[
+    window.location.toString().split("/").length - 1
   ];
-  const response = await fetch('/api/posts/upvote', {
-    method: 'PUT',
+  const response = await fetch("/api/posts/upvote", {
+    method: "PUT",
     body: JSON.stringify({
-      post_id: id
+      post_id: id,
     }),
     headers: {
-      'Content-Type': 'application/json'
-    }
+      "Content-Type": "application/json",
+    },
   });
 
   if (response.ok) {
@@ -21,4 +22,6 @@ async function upvoteClickHandler(event) {
   }
 }
 
-document.querySelector('.upvote-btn').addEventListener('click', upvoteClickHandler);
+document
+  .querySelector("#like-button")
+  .addEventListener("click", upvoteClickHandler);
