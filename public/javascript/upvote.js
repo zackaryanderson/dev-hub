6743e0fe3f0258ff56be9657e948e784bb6337e9
zanderson1998 <zanderson1998@yahoo.com>
2021-03-04 12:55:55 -0700

    remove error for likingposts

--- public/javascript/upvote.js
+++ public/javascript/upvote.js
@@ -18,7 +18,8 @@ async function upvoteClickHandler(event) {
   if (response.ok) {
     document.location.reload();
   } else {
-    alert(response.statusText);
+    //alert(response.statusText);
+    document.location.reload();
   }
 }
 
