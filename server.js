5d4e2c159fa587b9bd1a6c5b5507df7aa33255b2
zanderson1998 <zanderson1998@yahoo.com>
2021-03-04 13:36:00 -0700

    change secret

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const session = require('express-session');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 //cookies
 const sess = {
-    secret: SECURE_KEY,
+    secret: process.env.SECURE_KEY,
     cookie: { maxAge: 150000 },
     resave: false,
     rolling: true,
